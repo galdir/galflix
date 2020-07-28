f18a6cf89407a8d5687d06b9fc30c3221236444e
Galdir Reges <galdir@gmail.com>
2020-07-28 11:57:50 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,20 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      {/* <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a> */}
+      <a href="https://github.com/galdir">
+        {/* <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" /> */}
+        Github do Galdir
       </a>
-      <p>
+      {/* <p>
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
-      </p>
+      </p> */}
     </FooterBase>
   );
 }
